c5803ac9a63b92099aedf5f1f47bc516e18bb041
Sj1218305 <jainshobit0@gmail.com>
2020-08-18 23:58:55 +0530

    Updates

---
+++ precache-manifest.17d80bfd8051cc93c7ca2b198f87ddef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8145556ed856fc5acc1679b8a8c34b91",
+    "url": "/All-friends/index.html"
+  },
+  {
+    "revision": "2e9c3da9c8f7ef0302cc",
+    "url": "/All-friends/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "51b544a324450f3d51bf",
+    "url": "/All-friends/static/css/main.cfc4900b.chunk.css"
+  },
+  {
+    "revision": "2e9c3da9c8f7ef0302cc",
+    "url": "/All-friends/static/js/2.a0e717f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/All-friends/static/js/2.a0e717f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51b544a324450f3d51bf",
+    "url": "/All-friends/static/js/main.3cbed5fb.chunk.js"
+  },
+  {
+    "revision": "159743dd079b1b31b561",
+    "url": "/All-friends/static/js/runtime-main.f84bcc6d.js"
+  }
+]);
